01cce480ea55b0c1b30688f0deea5dd4d3f5f192
Baptiste CHERUEL <baptiste.cheruel@supinfo.com>
2025-01-24 11:32:05 +0100

    Test map view

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import React from "react";
+import MapView from 'react-native-maps';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [count, setCount] = React.useState(0);
+
+    return (
+        <MapView initialRegion={{
+            latitude: 49.12093718131425,
+            longitude: -0.19911361796825383
+        }}/>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
